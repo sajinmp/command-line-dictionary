af510cdf1101d0692fc0e2e3a810a6667212430c
sajinmp <sajinprasadkm@gmail.com>
2016-03-06 19:01:05 +0530

    game arrays converted to hash

--- functions/game.rb
+++ functions/game.rb
@@ -1,7 +1,21 @@
+def to_my_hash(arr)
+  h = {}
+  arr.each do |i|
+    unless i.nil?
+      h[i] = 0
+    end
+  end
+  return h
+end
+
 def game
   word = Wordnik.words.get_random_word['word']
-  defs = definition(word)
-  syns = synonym(word)
-  ants = antonym(word)
-  
+  defs = definition(word)[0]
+  syns = synonym(word)[0]
+  ants = antonym(word)[0]
+  exs = example(word)[0]
+  defs = to_my_hash(defs)
+  syns = to_my_hash(syns)
+  ants = to_my_hash(ants)
+  exs = to_my_hash(exs)
 end
